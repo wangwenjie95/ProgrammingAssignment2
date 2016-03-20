388e60e344ca498a7f3d790e04bf5a3f559410a7
wangwenjie95 <wangwenjie95@163.com>
2016-03-20 20:47:47 +0800

    update

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function()x
+  setmean<-function(mean) 
+    m<<-mean
+  getmean<-function()m
+  list(set=set,get=get,
+       setmean=setmean,
+       getmean=getmean)
 
 }
 
@@ -11,5 +23,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  m<-x$getmean()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data<-x$get()
+  m<-men(data,...)
+  x$setmean(m)
+  m
         ## Return a matrix that is the inverse of 'x'
 }
